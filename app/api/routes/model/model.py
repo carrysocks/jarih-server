1d91a940a3c6e3e31e2720e8238dd4c596933d75
carrysocks <choi73737@gmail.com>
2024-04-18 11:15:50 +0900

    feat: refactor model api

--- app/api/routes/model/model.py
+++ app/api/routes/model/model.py
@@ -5,65 +5,33 @@ import httpx
 
 router = APIRouter()
 
-async def call_lambda_api(pre_values, route_id, lambda_endpoint):
-    async with httpx.AsyncClient() as client:
-        response = await client.post(
-            lambda_endpoint, 
-            json={"pre_values": pre_values, "route_id": route_id})
+async def call_lambda_api(input_data, route_id, lambda_endpoint):
+    with httpx.Client() as client:
+        response = client.post(
+            lambda_endpoint,
+            json={"input_data": input_data, "route_id": route_id}
+        )
         response.raise_for_status()
         return response.json()
 
-@router.post("/predict")
+@router.post("/model/predict")
 async def predict(
     route_id: str,
     model_service: ModelService = Depends(ModelService),
     settings = Depends(get_app_settings)
 ):
-    result = model_service.get_bus_window_data(route_id=route_id)
-    
-    station_datas = model_service.get_station_data(route_id=route_id)
-    station_sequences = [[] for _ in range(station_datas["length"])]
-    for plate_no, values in result.items():
-        pre_values = [value[:6] for value in values]
-
-        print(plate_no, values)
-        station_id = values[-1][6]
-        station_seq = values[-1][0]
-        prediction_values = await call_lambda_api(pre_values, route_id, settings.LAMBDA_ENDPOINT)
-
-        for i, v in enumerate(prediction_values):
-            if station_seq+i >= station_datas["length"]:
-                break
-            station_sequences[station_seq+i].insert(0, v)    
-            
-    for i, station_sequence in enumerate(station_sequences):
-        if not station_sequence: 
-            station_sequences[i] = [None]
-
-    return station_sequences
-
-@router.post("/predict_ver2")
-async def predict(
-    route_id: str,
-    model_service: ModelService = Depends(ModelService),
-    settings = Depends(get_app_settings)
-):
-    result = model_service.get_bus_window_data(route_id=route_id)
-    
+    bus_window_data = model_service.get_bus_window_data(route_id=route_id)
     predictions = {}
-    for plate_no, values in result.items():
-        pre_values = [value[:6] for value in values]
 
-        print(plate_no, values)
-        station_id = values[-1][6]
-        station_seq = values[-1][0]
-        
-        prediction_values = await call_lambda_api(pre_values, route_id, settings.LAMBDA_ENDPOINT)
-        
+    for plate_no, plate_no_values in bus_window_data.items():
+        input_data = [value[:6] for value in plate_no_values]
+        station_id = plate_no_values[-1][6]
+        station_seq = plate_no_values[-1][0]
+        prediction_values = await call_lambda_api(input_data, route_id, settings.LAMBDA_ENDPOINT)
         predictions[plate_no] = [{
             "predictions": prediction_values,
             "station_id": station_id,
             "station_seq": station_seq
         }]
-        
+
     return {"predictions": predictions}
--- app/db/repository/model/model.py
+++ app/db/repository/model/model.py
@@ -8,6 +8,7 @@ from collections import defaultdict
 from app.db.models.bus_stop import TblBusStop
 from app.services.bus import BusService
 
+from datetime import datetime
 
 class ModelRepository:
     def __init__(
@@ -19,54 +20,39 @@ class ModelRepository:
         self._session = session
 
     def get_latest_buses_by_route_and_plate(self, route_id: str):
-        # Get the current locations of all buses in operation
         current_bus_locations = self.bus_service.get_bus_location_list(route_id)
-
-        # Use a dictionary to store the latest data of each bus
+        
         grouped_results = defaultdict(list)
-
-        # For each bus in operation
+        
         for bus_location in current_bus_locations:
             plate_no = bus_location.plateNo
-
-            # Get the latest data of this bus
-            latest_data = (
-                self._session.query(BusData)
-                .filter_by(route_id=route_id, plate_no=plate_no)
-                .order_by(BusData.date_time.desc())
-                .limit(100)
-                .all()
-            )
-
-            # Store the latest data of this bus in the dictionary
-            for data in latest_data:
-                data_time = data.date_time
-                hour = data_time.hour
-                minute = data_time.minute // 10
-                day_of_week = data_time.weekday()
-                is_weekend = 1 if day_of_week in [5, 6] else 0
-
-                if (
-                    not any(
-                        data.station_order == entry[0]
-                        for entry in grouped_results[plate_no]
-                    )
-                    and len(grouped_results[plate_no]) < 5
-                ):
-                    grouped_results[plate_no].append(
-                        [
-                            int(data.station_order),
-                            hour,
-                            minute,
-                            day_of_week,
-                            int(data.plate_type),
-                            is_weekend,
-                            data.station_id,
-                            int(data.station_order),
-                        ]
-                    )
-
+            date_time = datetime.now() 
+            
+            hour = date_time.hour
+            minute = date_time.minute // 10
+            day_of_week = date_time.weekday()
+            is_weekend = 1 if day_of_week in [5, 6] else 0
+            
+            if (
+                not any(bus_location.stationSeq == entry[0] for entry in grouped_results[plate_no])
+                and len(grouped_results[plate_no]) < 5
+            ):
+                grouped_results[plate_no].append(
+                    [
+                        int(bus_location.stationSeq),
+                        hour,
+                        minute,
+                        day_of_week,
+                        int(bus_location.plateType),
+                        is_weekend,
+                        bus_location.station_id,
+                        int(bus_location.stationSeq),
+                    ]
+                )
+        
+        for plate_no in grouped_results:
             grouped_results[plate_no].reverse()
+        
         return grouped_results
 
     def get_station_data(self, route_id: str):
